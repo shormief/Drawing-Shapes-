0fa76937868a2cd61af187a817f90af70338c325
shormiejfaruque <faruqueshormie@gmail.com>
2016-08-31 15:50:54 -0400

    Shapes code

---
+++ Shapes.py
@@ -0,0 +1,34 @@
+from turtle import *
+for i in range (100):
+	speed(100)
+	pendown()
+	pencolor("BlueViolet")
+	for i in range(4):	
+		forward(100)
+		right(90)
+	penup()
+	forward(-200)
+	right(10)
+	pendown()
+	pencolor("blue3")
+	for i in range(4): 
+		forward(100)
+		right(120)
+	penup()
+	back(200)
+	right(10)
+	pendown()
+	pencolor("aquamarine3")	
+	for i in range (5):
+		forward(100)
+		right(72)
+	penup() 
+	forward(100)
+	right(10)
+	pendown()
+	pencolor("coral")
+	for i in range (6):
+		forward(100)
+		right(60)
+	right(10)
+		
